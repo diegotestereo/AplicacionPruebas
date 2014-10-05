4d0c3f07e4e966fc2209fa12cce0b5e571d0c005
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2014-10-05 16:33:12 -0300

    se corrigieron errores
    arhoa funciona perfectamante

--- src/com/example/aplicacionespruebas/MainActivity.java
+++ src/com/example/aplicacionespruebas/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.aplicacionespruebas;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -12,25 +16,62 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MiServicio.ACTIVIDAD=this;
+        try {
+        	Log.i(getClass().getSimpleName(), "Iniciando el servicio de la actividad");
+        	Intent servicio = new Intent(this,MiServicio.class);
+        	
+        	if(startService(servicio)==null)
+        	{
+        		this.notificar("No se ha podido iniciar el  servicio");
+        	}
+        	else{
+        		this.notificar("Servicio iniciado Correctamente");
+        	}
+		} catch (Exception e) {
+			this.notificar(e.getMessage());
+		}
     }
 
+  @Override
+protected void onDestroy() {
+	// TODO Auto-generated method stub
+	super.onDestroy();
+	  try {
+		  Log.i(getClass().getSimpleName(), "Finalizando el servicio de la actividad");
+      	Intent servicio = new Intent(this,MiServicio.class);
+      	
+      	if(stopService(servicio)){
+      		this.notificar("Se finalizo el servicio perfectamente");
+      	}
+      	else{
+      		this.notificar("No se a podido finalizar el servicio");
+      	}
+      	
+      	try {
+			this.finalize();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+      	
+		} catch (Exception e) {
+			this.notificar(e.getMessage());
+		}
+	
+}
+    
+    
+    private void notificar(String cadena) {
+	Context contexto =getApplicationContext();
+	CharSequence texto= cadena;
+	int duration= Toast.LENGTH_SHORT;
+	
+	Toast toast=Toast.makeText(contexto, texto, duration);
+	toast.show();
+	
+		
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
